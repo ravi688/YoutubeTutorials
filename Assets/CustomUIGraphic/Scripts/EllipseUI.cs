b8d811570f0d5f3a97aadc9b984357549f17448a
Ravi Prakash Singh <rp0412204@gmail.com>
2022-01-30 21:29:31 +0530

    Now it's working

--- Assets/CustomUIGraphic/Scripts/EllipseUI.cs
+++ Assets/CustomUIGraphic/Scripts/EllipseUI.cs
@@ -3,20 +3,36 @@ using UnityEngine.UI;
 
 public class EllipseUI : Graphic
 {
+    [SerializeField]
+    private float thickness = 40;
+    [SerializeField]
+    private int divisions = 20;
+    [SerializeField]
+    private float minorRadius = 200;
+    [SerializeField]
+    private float majorRadius = 300;
+
     protected override void OnPopulateMesh(VertexHelper vh)
     {
         base.OnPopulateMesh(vh);
+        vh.Clear();
         UIVertex vertex = UIVertex.simpleVert;
-        vertex.position = new Vector2(100, 100);         // top right corner
-        vh.AddVert(vertex);
-        vertex.position = new Vector3(-100, 100);        // top left corner
-        vh.AddVert(vertex);
-        vertex.position = new Vector3(-100, -100);       // bottom left corner
-        vh.AddVert(vertex);
-        vertex.position = new Vector3(100, -100);        // bottom right corner
+        float deltaAngle = Mathf.PI * 2 / divisions;
+        int vertexCount = divisions * 2;
+        for(int i = 0; i < divisions; i++)
+        {
+            float angle = i * deltaAngle;
+            // bottom right corner
+            vertex.position = new Vector2(Mathf.Sin(angle) * (majorRadius - thickness), Mathf.Cos(angle) * (minorRadius - thickness));
+            vh.AddVert(vertex);
 
+            // top right corner
+            vertex.position = new Vector2(Mathf.Sin(angle) * majorRadius, Mathf.Cos(angle) * minorRadius);
+            vh.AddVert(vertex);
 
-        vh.AddTriangle(0, 1, 2);
-        vh.AddTriangle(2, 3, 0);
+            int offset = i * 2;
+            vh.AddTriangle(offset + 0, offset + 1, (offset + 3) % vertexCount);
+            vh.AddTriangle((offset + 3) % vertexCount, (offset + 2) % vertexCount, offset + 0);
+        }
     }
 }

4351a13ddd48236b1f28813bfa2ef4ee0d162570
Ravi Prakash Singh <rp0412204@gmail.com>
2022-01-30 21:49:42 +0530

    Support for resize with rect transform

--- Assets/CustomUIGraphic/Scripts/EllipseUI.cs
+++ Assets/CustomUIGraphic/Scripts/EllipseUI.cs
@@ -7,15 +7,16 @@ public class EllipseUI : Graphic
     private float thickness = 40;
     [SerializeField]
     private int divisions = 20;
-    [SerializeField]
-    private float minorRadius = 200;
-    [SerializeField]
-    private float majorRadius = 300;
-
+   
     protected override void OnPopulateMesh(VertexHelper vh)
     {
         base.OnPopulateMesh(vh);
         vh.Clear();
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        float majorRadius = rectTransform.rect.width * 0.5f;
+        float minorRadius = rectTransform.rect.height * 0.5f;
+        Vector2 center = Vector2.one * 0.5f - rectTransform.pivot;
+        center = new Vector2(center.x * majorRadius * 2, center.y * minorRadius * 2);
         UIVertex vertex = UIVertex.simpleVert;
         float deltaAngle = Mathf.PI * 2 / divisions;
         int vertexCount = divisions * 2;
@@ -23,11 +24,11 @@ public class EllipseUI : Graphic
         {
             float angle = i * deltaAngle;
             // bottom right corner
-            vertex.position = new Vector2(Mathf.Sin(angle) * (majorRadius - thickness), Mathf.Cos(angle) * (minorRadius - thickness));
+            vertex.position = new Vector2(Mathf.Sin(angle) * (majorRadius - thickness), Mathf.Cos(angle) * (minorRadius - thickness)) + center;
             vh.AddVert(vertex);
 
             // top right corner
-            vertex.position = new Vector2(Mathf.Sin(angle) * majorRadius, Mathf.Cos(angle) * minorRadius);
+            vertex.position = new Vector2(Mathf.Sin(angle) * majorRadius, Mathf.Cos(angle) * minorRadius) + center;
             vh.AddVert(vertex);
 
             int offset = i * 2;
